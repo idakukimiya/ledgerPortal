9f63388e841865629eb56be4869324d175b9bd92
idakukimiya <kukimiya.ida@gmail.com>
2022-09-26 10:45:51 -0400

    fixing syntax

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
-import GET_ME from '../utils/queries';
+import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
